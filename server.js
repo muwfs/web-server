f958be86155b640ba61ee6c0f0ca03ee250f3146
Watcharapon Fongsak <wfongsak@gmail.com>
2015-11-28 22:07:19 +0700

    add middleware page

--- server.js
+++ server.js
@@ -7,19 +7,7 @@ var PORT = 3000;
 // 	res.send('Hello Express!');
 // });
 
-var middleware = {
-	requireAuthentication: function (req, res, next) {
-		console.log('private route hit!');
-		next();
-	},
-	logger: function (req, res, next){
-
-		//new Date().toString()
-
-		console.log('Request: '+new Date().toString() +' '+ req.method + ' ' + req.originalUrl);
-		next();
-	}
-};
+var middleware = require('./middleware.js')
 
 app.use(middleware.logger);
 //app.use(middleware.requireAuthentication); 
